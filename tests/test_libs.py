ed9f085f5449a9c7b5f8b29d76cea08e554629ee
thanos vassilakis <thanos@syntazo.com>
2020-12-05 00:12:55 -0500

    Update test_libs.py

--- tests/test_libs.py
+++ tests/test_libs.py
@@ -3,15 +3,13 @@ import os
 from couchfs.couchfs import  CouchDBClient
 
 def test_get_attachment():
-    couch_uri ='couchdb://admin:R30d5tar%@127.0.0.1:5984/test'
-    with CouchDBClient(couch_uri).get_attachment('http://127.0.0.1:5984/test/TAKIS/takis/takis/wsgi.py') as fp:
+    with CouchDBClient().get_attachment('http://127.0.0.1:5984/test/TAKIS/takis/takis/wsgi.py') as fp:
         assert len(fp.read()) == 387
         assert os.path.exists(fp.name)
     assert os.path.exists(fp.name) == False
 
 
 def test_get_attachment_in_memory():
-    couch_uri ='couchdb://admin:R30d5tar%@127.0.0.1:5984/test'
-    with CouchDBClient(couch_uri).get_attachment('http://127.0.0.1:5984/test/TAKIS/takis/takis/wsgi.py',in_memory=True) as buff:
+    with CouchDBClient().get_attachment('http://127.0.0.1:5984/test/TAKIS/takis/takis/wsgi.py',in_memory=True) as buff:
         assert len(buff) == 387
     assert len(buff) == 387
